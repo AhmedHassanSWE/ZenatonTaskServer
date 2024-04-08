75ee5019f8bc270390c661f30b5bacf5681b5f7a
Ahmed Hassan <77825355+AhmedHassanSWE@users.noreply.github.com>
2024-04-08 18:34:29 +0200

    Update handle-link.js

--- utils/handle-link.js
+++ utils/handle-link.js
@@ -1,14 +1,14 @@
 const axios = require("axios");
 const externalId = "customer_test_1";
-module.exports = async (oldLink) => {
-  const response = await axios.post(
-    "https://api.pics.ee/v1/links/?access_token=20f07f91f3303b2f66ab6f61698d977d69b83d64",
-    {
-      url: oldLink,
-      externalId,
-    }
-  );
+module.exports = (oldLink) => {
+  // const response = await axios.post(
+  //   "https://api.pics.ee/v1/links/?access_token=20f07f91f3303b2f66ab6f61698d977d69b83d64",
+  //   {
+  //     url: oldLink,
+  //     externalId,
+  //   }
+  // );
 
-  const link = response.data.data.picseeUrl;
-  return link;
+  // const link = response.data.data.picseeUrl;
+  return oldLink;
 };
